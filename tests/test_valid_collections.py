eeac54c7b72b1f34537625c711089a7fe5e43e1e
Mark A. Miller <mamillerpa@gmail.com>
2023-04-28 07:59:17 -0400

    named_thing_set passing test

--- tests/test_valid_collections.py
+++ tests/test_valid_collections.py
@@ -4,14 +4,14 @@ import glob
 import unittest
 
 from linkml_runtime.loaders import yaml_loader
-from split_pool_mod_schema.datamodel.split_pool_mod_schema import DatabaseCollection
+from split_pool_mod_schema.datamodel.split_pool_mod_schema import Database
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples", "valid")
 
 EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR, '*.yaml'))
 
-MAIN_SCHEMA_CLASS_NAME = DatabaseCollection.class_name
+MAIN_SCHEMA_CLASS_NAME = Database.class_name
 
 ACCEPTABLE_PREFIX = DATA_DIR + "/" + MAIN_SCHEMA_CLASS_NAME
 
@@ -23,7 +23,7 @@ class TestData(unittest.TestCase):
         """Date test."""
         for path in EXAMPLE_FILES:
             if path.startswith(ACCEPTABLE_PREFIX):
-                obj = yaml_loader.load(path, target_class=DatabaseCollection)
+                obj = yaml_loader.load(path, target_class=Database)
                 assert obj
             else:
                 print()
